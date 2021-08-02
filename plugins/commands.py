7b1dff880e0a53baed1e5cbc20aeb415fe84e4f8
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-08-02 04:12:17 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,7 +40,7 @@ async def start(bot, message):
     else:
         buttons = [
             [
-                InlineKeyboardButton('Updates Channel 🗣', url='https://t.me/sl_bot_zone'),
+                InlineKeyboardButton('Updates Channel 🗣', url='https://t.me/joinchat/44HZBE6gOxhlNjFl'),
                 InlineKeyboardButton('Go Inline 🎭', switch_inline_query=''),
             ],
             [
